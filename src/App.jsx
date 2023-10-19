f7e38f0a2bcf2a283d5da48b1baf630d85aaa543
SáńchezBetzabe <betzabegothic@gmail.com>
2023-10-18 21:03:51 -0600

    Prueba MammaMia!

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./screen/Home/Home";
 import PizzaDetail from "./screen/PizzaDetail";
 import Cart from "./screen/Cart";
